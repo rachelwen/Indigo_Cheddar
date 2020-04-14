2fa46371d3d4e6a621b95af772c2d4e61340c36b
rachelwen <rachelwen823@gmail.com>
2020-04-13 23:17:28 -0700

    new particle class (under construction)
    trying a new particle class that uses perlin noise + various aspects of sound to control the direction of particles. under construction

--- particle.js
+++ particle.js
@@ -2,6 +2,7 @@ class Particle{
   constructor(x,y,soundVel,R,G,B){
   this.pos = createVector(x,y);
   this.vel = createVector(0,soundVel);
+  //this.vel = p5.Vector.random2D();
   this.acc = createVector(0,0);
   this.R = R;
   this.G = G;
@@ -29,9 +30,77 @@ class Particle{
     //stroke(colorVal,colorVal,colorVal);
   //strokeWeight(5)
     //point(this.pos.x, this.pos.y);
-    fill(this.R,this.G,this.B,2);
+    fill(this.R,this.G,this.B,96);
     noStroke();
     rect(this.pos.x,this.pos.y,15,40);
     //circle(this.pos.x,this.pos.y,20,20);
   }
+}
+
+
+
+class NewParticle{
+  constructor(soundVel,R,G,B){
+    this.pos = 
+    createVector(random(width),random(height));
+    this.vel = p5.Vector.random2D();
+   // this.vel = createVector(0,soundVel);
+    this.acc = createVector(0,0);
+   // this.maxSpeed = soundVel;
+   this.maxSpeed = 4;
+    this.prevPos = this.pos.copy();
+    this.R = R;
+    this.G = G;
+    this.B = B;
+  }
+
+  update(){
+    this.vel.add(this.acc);
+    this.vel.limit(this.maxSpeed);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+}
+
+  follow(vectors){
+    var x = floor(this.pos.x/scl);
+    var y = floor(this.pos.y/scl);
+    var index = x + y * cols;
+    var force = vectors[index];
+    this.applyForce(force);
+  }
+
+  applyForce(force){
+    this.acc.add(force);
+  }
+
+  show(){
+    stroke(this.R,this.G,this.B);
+    strokeWeight(5)
+    line(this.pos.x,this.pos.y,this.prevPos.x,this.prevPos.y)
+    this.updatePrev();
+  }
+
+  updatePrev(){
+    this.prevPos.x = this.pos.x;
+    this.prevPos.y = this.pos.y;
+  }
+  edges(){
+    if (this.pos.x > width ) {
+      this.pos.x = 0;
+      this.updatePrev();
+    }
+    if (this.pos.x < 0 ) {
+      this.pos.x = width;
+       this.updatePrev();
+    }
+    if (this.pos.y > height ) {
+      this.pos.y = 0;
+       this.updatePrev();
+    }
+    if (this.pos.y < 0 ) {  
+    this.pos.y = height;
+       this.updatePrev();
+  }
+  }
+
 }
--- sketch.js
+++ sketch.js
@@ -16,6 +16,16 @@ let bassBars = [];
 let midBars = [];
 let highBars = [];
 
+let inc = 0.1;
+let scl = 20;
+let rows, cols;
+let zoff = 0;
+let fr;
+let newParticles = [];
+let flowField;
+
+
+
 function toggleSong() {
   if (song.isPlaying()) {
     song.pause();
@@ -25,17 +35,32 @@ function toggleSong() {
 }
 
 function preload() {
-  song = loadSound('test.mp3');
+  song = loadSound('sickelectronicsong2.mp3');
 }
 
 function setup() {
   createCanvas(600, 400);
+
+  cols = floor(width / scl);
+  rows = floor(height / scl);
+  flowField = new Array(cols*rows);
+  // for(var i = 0; i < 2000; i++){
+  //   newParticles[i] = new NewParticle();
+  //   }
+
+//////////////////////////
+
+
+
+
+
   button = createButton('toggle');
   button.mousePressed(toggleSong);
- // song.play();
- background(0);
+  //song.play();
+//background(0);
   
-  fft = new p5.FFT(0.90, 64);
+  //fft = new p5.FFT(0.90, 64);
+  fft = new p5.FFT(0.99, 64);
 
   amp = new p5.Amplitude();
   
@@ -61,28 +86,74 @@ function resetCanvas(){
 }
 
 function draw() {
+  background(0)
   
+
+
+
   let spectrum = fft.analyze();
   let bassVal = fft.getEnergy('bass');
-  soundVel= map(bassVal, 0, 240, 3, 12);
+  soundVel= map(bassVal, 0, 240, 1, 2);
+ 
+
 
   let midVal = fft.getEnergy('highMid');
   soundVel2= map(midVal, 0, 240, 3, 12);
 
   let highVal = fft.getEnergy('treble');
-  soundVel3 = map(highVal, 0, 240, 3, 12);
-  
+  soundVel3 = map(highVal, 0, 240, 2, 20);
+  let circleRad = map(highVal,0,440,0,80)
+  console.log(highVal)
 
 
 
+  let yoff = 0;
+  for(let y = 0; y < rows; y++){
+    let xoff = 0;
+    for(let x = 0; x < cols; x++){
+      let index = (x + y * cols);
+      //let angle = noise(xoff, yoff, zoff) * TWO_PI*4;
+      let angle = noise(xoff, yoff, soundVel3) * TWO_PI;
+     // let angle = soundVel*TWO_PI*4;
+     
+      let v = p5.Vector.fromAngle(angle);
+      v.setMag(1);
+      flowField[index] = v;
+      xoff += inc;
+      stroke(255);
+      strokeWeight(1);
+      push()
+      translate(x * scl, y * scl)
+      rotate(v.heading());
+      
+     line(0, 0, scl, 0)
+     //ellipse(0,0,ampMap,ampMap)
+     
+      pop()
+    }
+    yoff += inc;
+    zoff += 0.0004;
+    
+  }
+
+  for(let j = 0; j < newParticles.length; j++){
+    newParticles[j].follow(flowField);
+    newParticles[j].edges();
+    newParticles[j].show();
+    newParticles[j].update();
+  }
 
 
 
-
-  if (random(1)<0.1){
-    bassBars.push(new Firework(-soundVel,random(100,255),0,random(100,255)));
+  if (random(1)<0.2){
+    // bassBars.push(new Firework(-soundVel,random(100,255),0,random(100,255)));
+   // bassBars.push(new NewParticle(soundVel,200,2,0))
    }
   for (let i = bassBars.length-1; i >=0; i --){
+    if(bassVal>0 ){
+    bassBars[i].follow(flowField)
+    }
+    bassBars[i].edges();
     bassBars[i].update();
     bassBars[i].show();
   }
@@ -90,21 +161,24 @@ function draw() {
   
 
  
-  if (random(1)<0.1){
-    midBars.push(new Firework(-soundVel2,0,random(100,255),random(100,255)));
+  if (random(1)<0.1 && midVal>0){
+   // midBars.push(new Firework(-soundVel2,0,random(100,255),random(100,255)));
+   //midBars.push(new NewParticle(soundVel,0,200,0))
    }
   for (let i = midBars.length-1; i >=0; i --){
+    midBars[i].follow(flowField);
+    midBars[i].edges();
     midBars[i].update();
     midBars[i].show();
   }
 
-  if (random(1)<0.1){
-    highBars.push(new Firework(-soundVel3,255,255,255));
-   }
-  for (let i =  highBars.length-1; i >=0; i --){
-    highBars[i].update();
-    highBars[i].show();
-  }
+  // if (random(1)<0.1){
+  //   highBars.push(new Firework(-soundVel3,255,255,255));
+  //  }
+  // for (let i =  highBars.length-1; i >=0; i --){
+  //   highBars[i].update();
+  //   highBars[i].show();
+  // }
 
   
   
